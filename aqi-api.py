a698e4335d4db6fabf1fd516a0e4906462de553c
Jake Dexheimer <jakedex@gmail.com>
2016-02-05 01:56:13 +0000

    Scraping working, hour intervals function started

--- aqi-api.py
+++ aqi-api.py
@@ -1,34 +1,48 @@
 from flask import Flask
-from bs4 import BeautifulSoup
-import urllib
+from flask import jsonify
+from datetime import datetime
+import dryscrape
 
 app = Flask(__name__)
 
 site = "http://aqicn.org/city/"
-cities = ["beijing", "shanghai", "hongkong", "bangkok", "delhi", "mumbai",
-          "tokyo", "seoul", "manila", "guangzhou"]
-aqi = {"beijing":-1, "shanghai":-1, "hongkong":-1, "bangkok":-1, "delhi":-1,
+data = {"beijing":-1, "shanghai":-1, "hongkong":-1, "bangkok":-1, "delhi":-1,
        "mumbai":-1, "tokyo":-1, "seoul":-1, "manila":-1, "guangzhou":-1}
+response = None
+last_fetch = None
+
+dryscrape.start_xvfb()
+sess = dryscrape.Session(base_url = site)
+sess.set_attribute('auto_load_images', False)
+
+def fetch():
+    last_fetch = datetime.now()
+    
+    for city in data.keys():
+        sess.visit(city + '/m')
+        ele = sess.at_xpath('//*[@id="xatzcaqv"]')
+        data[city] = ele.text()
+        print(city + ": " + ele.text())
+
+    response = jsonify(**data)
+    print(response)
+
+def one_hour():
+    return (datetime.now() - last_fetch).hours > 0
+
+def get_data():
+    if(last_fetch == None or one_hour):
+        fetch()
+
+    return response
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
 @app.route("/aqi")
-def fetch():
-#    for city in cities:
-#        page = requests.get(site+city)
-#        tree = html.fromstring(page.content)
-#        aqis = tree.xpath('//*[@id="citydivmain"]/div/div/div/table[2]/tbody/tr/td[1]/div')
-#        print("{} aqi: {}".format(city, aqis))
-    url = "http://aqicn.org/city/beijing/m/"
-    page = urllib.urlopen(url)
-    soup = BeautifulSoup(page.read())
-#    print(soup.get_text().encode('utf-8'))
-    div = soup.find(id="header").table
-    print(div)
-    return "con"
-    
+def aqi():
+    return get_data()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
